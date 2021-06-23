78ba377cf9fc3e0b2728af8851c0271b1167514a
Billy Jacoby <billyjacoby@gmail.com>
2021-06-23 19:59:49 -0400

    functions working, refactor a bit next

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,15 +3,38 @@ import "./Calculator.css";
 
 const Calculator = () => {
   const [screen, setScreen] = useState(0);
+  const [prevValue, setPrevValue] = useState();
+  const [storedOp, setStoredOp] = useState();
+
+  console.log("screen ", screen);
+  console.log("prevValue ", prevValue);
 
   const setScreenFunction = (value) => {
-    if (value === "clear") return setScreen(0);
+    /* Disabling ES lint rule here due to clearing all state */
+    // eslint-disable-next-line
+    if (value === "clear")
+      return setScreen(0), setPrevValue(undefined), setStoredOp();
     if (screen === 0) {
       if (typeof value === "number") {
         setScreen(value);
       }
     } else {
       setScreen("" + screen + value);
+    }
+  };
+
+  const startOperation = (operation) => {
+    setPrevValue(parseFloat(screen));
+    if (operation === "equals") {
+      // TODO: Must fix pressing equals sign in succession
+      console.log("equals");
+      if (typeof storedOp === "function") {
+        setScreen(storedOp(prevValue, screen));
+        setPrevValue(screen);
+      }
+    } else {
+      setStoredOp(() => operation);
+      setScreen(0);
     }
   };
   return (
@@ -28,7 +51,14 @@ const Calculator = () => {
         </button>
         <button className="button function">+/-</button>
         <button className="button function">%</button>
-        <button className="button operation">÷</button>
+        <button
+          className="button operation"
+          onClick={() =>
+            startOperation((prevValue, value) => prevValue / value)
+          }
+        >
+          ÷
+        </button>
       </div>
       <div className="button-row">
         <button className="button number" onClick={() => setScreenFunction(7)}>
@@ -40,7 +70,14 @@ const Calculator = () => {
         <button className="button number" onClick={() => setScreenFunction(9)}>
           9
         </button>
-        <button className="button operation">×</button>
+        <button
+          className="button operation"
+          onClick={() =>
+            startOperation((prevValue, value) => prevValue * value)
+          }
+        >
+          ×
+        </button>
       </div>
       <div className="button-row">
         <button className="button number" onClick={() => setScreenFunction(4)}>
@@ -52,7 +89,14 @@ const Calculator = () => {
         <button className="button number" onClick={() => setScreenFunction(6)}>
           6
         </button>
-        <button className="button operation">-</button>
+        <button
+          className="button operation"
+          onClick={() =>
+            startOperation((prevValue, value) => prevValue - value)
+          }
+        >
+          -
+        </button>
       </div>
       <div className="button-row">
         <button className="button number" onClick={() => setScreenFunction(1)}>
@@ -64,7 +108,14 @@ const Calculator = () => {
         <button className="button number" onClick={() => setScreenFunction(3)}>
           3
         </button>
-        <button className="button">÷</button>
+        <button
+          className="button"
+          onClick={() =>
+            startOperation((prevValue, value) => prevValue + value)
+          }
+        >
+          +
+        </button>
       </div>
       <div className="button-row">
         <button
@@ -79,7 +130,12 @@ const Calculator = () => {
         >
           .
         </button>
-        <button className="button operation">=</button>
+        <button
+          className="button operation"
+          onClick={() => startOperation("equals")}
+        >
+          =
+        </button>
       </div>
     </div>
   );
