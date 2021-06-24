f42bff3e05fb8f3e57f593cc738a25c2f3477fae
Billy Jacoby <billyjacoby@gmail.com>
2021-06-24 18:21:24 -0400

    updated styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,50 +3,48 @@ import "./Calculator.css";
 import Button from "./Button";
 
 const Calculator = () => {
-  const [screen, setScreen] = useState(0);
+  const [curValue, setCurValue] = useState(0);
   const [prevValue, setPrevValue] = useState();
   const [storedOp, setStoredOp] = useState();
 
-  console.log("screen ", screen);
+  console.log("curValue ", curValue);
   console.log("prevValue ", prevValue);
 
-  const setScreenFunction = (value) => {
-    /* Disabling ES lint rule here due to clearing all state */
+  const setCurValueFunction = (value) => {
     if (value === "clear")
+      /* Disabling ES lint rule here due to clearing all state */
       // eslint-disable-next-line
-      return setScreen(0), setPrevValue(undefined), setStoredOp();
-    if (screen === 0) {
+      return setCurValue(0), setPrevValue(undefined), setStoredOp();
+    if (curValue === 0) {
       if (typeof value === "number") {
-        setScreen(value);
+        setCurValue(value);
       }
     } else {
-      setScreen("" + screen + value);
+      setCurValue("" + curValue + value);
     }
   };
 
   const startOperation = (operation) => {
-    setPrevValue(parseFloat(screen));
-    if (operation === "equals") {
-      // TODO: Must fix pressing equals sign in succession
-      console.log("equals");
-      if (typeof storedOp === "function") {
-        setScreen(storedOp(prevValue, screen));
-        setPrevValue(screen);
-      }
-    } else {
-      setStoredOp(() => operation);
-      setScreen(0);
+    setPrevValue(parseFloat(curValue));
+    setStoredOp(() => operation);
+    setCurValue(0);
+  };
+
+  const equalPressed = () => {
+    if (typeof storedOp === "function") {
+      setCurValue(storedOp(prevValue, curValue));
+      setPrevValue(curValue);
     }
   };
   return (
     <div className="wrapper">
       <div className="display">
-        <span>{screen}</span>
+        <span>{curValue}</span>
       </div>
       <div className="button-row">
         <Button
           type="function"
-          onClick={() => setScreenFunction("clear")}
+          onClick={() => setCurValueFunction("clear")}
           label="AC"
         />
         <Button type="function" label="+/-" />
@@ -60,9 +58,21 @@ const Calculator = () => {
         />
       </div>
       <div className="button-row">
-        <Button type="number" onClick={() => setScreenFunction(7)} label="7" />
-        <Button type="number" onClick={() => setScreenFunction(8)} label="8" />
-        <Button type="number" onClick={() => setScreenFunction(9)} label="9" />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(7)}
+          label="7"
+        />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(8)}
+          label="8"
+        />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(9)}
+          label="9"
+        />
         <Button
           type="operation"
           onClick={() =>
@@ -72,9 +82,21 @@ const Calculator = () => {
         />
       </div>
       <div className="button-row">
-        <Button type="number" onClick={() => setScreenFunction(4)} label="4" />
-        <Button type="number" onClick={() => setScreenFunction(5)} label="5" />
-        <Button type="number" onClick={() => setScreenFunction(6)} label="6" />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(4)}
+          label="4"
+        />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(5)}
+          label="5"
+        />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(6)}
+          label="6"
+        />
         <Button
           type="operation"
           onClick={() =>
@@ -84,9 +106,21 @@ const Calculator = () => {
         />
       </div>
       <div className="button-row">
-        <Button type="number" onClick={() => setScreenFunction(1)} label="1" />
-        <Button type="number" onClick={() => setScreenFunction(2)} label="2" />
-        <Button type="number" onClick={() => setScreenFunction(3)} label="3" />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(1)}
+          label="1"
+        />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(2)}
+          label="2"
+        />
+        <Button
+          type="number"
+          onClick={() => setCurValueFunction(3)}
+          label="3"
+        />
         <Button
           type="operation"
           onClick={() =>
@@ -97,18 +131,18 @@ const Calculator = () => {
       </div>
       <div className="button-row">
         <Button
-          type="number double"
-          onClick={() => setScreenFunction(0)}
+          type="number double bottom-left"
+          onClick={() => setCurValueFunction(0)}
           label="0"
         />
         <Button
           type="number"
-          onClick={() => setScreenFunction(".")}
+          onClick={() => setCurValueFunction(".")}
           label="."
         />
         <Button
-          type="equals"
-          onClick={() => startOperation("equals")}
+          type="operation bottom-right"
+          onClick={equalPressed}
           label="="
         />
       </div>
